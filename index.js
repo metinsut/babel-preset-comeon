aa32aa838c9060427761065616bdc13d7fc9a8cd
Dilip Kr. Shukla <contact@fusionstrings.com>
2019-07-15 11:49:15 +0200

    make useBuiltIns usage default, remove polyfill, add targets

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 module.exports = (api, options) => {
+  const {
+    debug = false,
+    modules = "auto",
+    shippedProposals = true,
+    spec = true,
+    useBuiltIns = "usage",
+    targets
+  } = options;
 
-	const {debug = false, modules = 'auto', shippedProposals = true, spec = true, useBuiltIns = 'entry'} = options;
-	
-	return {
-		presets: [
-			[
-				require('@babel/preset-env'),
-				{
-					debug,
-					modules,
-					shippedProposals,
-					spec,
-					useBuiltIns,
-					corejs: {
-						version: 3,
-						proposals: true
-					}
-				}
-			],
-			require('@babel/preset-react'),
-			require('@babel/preset-typescript')
-		].filter(Boolean),
-		plugins: [
-			require('babel-plugin-styled-components'),
-			require('@babel/plugin-transform-react-constant-elements'),
-			require('@babel/plugin-proposal-class-properties'),
-			require('@babel/plugin-proposal-json-strings'),
-			require('@babel/plugin-syntax-dynamic-import'),
-			require('@babel/plugin-syntax-import-meta'),
-			api.env('test') && require('babel-plugin-dynamic-import-node')
-		].filter(Boolean)
-	}
+  return {
+    presets: [
+      [
+        require("@babel/preset-env"),
+        {
+          debug,
+          modules,
+          shippedProposals,
+          spec,
+		  useBuiltIns,
+		  targets,
+          corejs: {
+            version: 3,
+            proposals: true
+          }
+        }
+      ],
+      require("@babel/preset-react"),
+      require("@babel/preset-typescript")
+    ].filter(Boolean),
+    plugins: [
+      require("babel-plugin-styled-components"),
+      require("@babel/plugin-transform-react-constant-elements"),
+      require("@babel/plugin-proposal-class-properties"),
+      require("@babel/plugin-proposal-json-strings"),
+      require("@babel/plugin-syntax-dynamic-import"),
+      require("@babel/plugin-syntax-import-meta"),
+      api.env("test") && require("babel-plugin-dynamic-import-node")
+    ].filter(Boolean)
+  };
 };
